f68db3999a30f6862e07d5de3f368e326b8e80fb
Haoyu.Chen <null-qwerty@outlook.com>
2026-01-13 14:24:54 +0800

    feat: add wheel leg env config

---
+++ source/wheel_leg_rl/wheel_leg_rl/env_cfg/wheelleg.py
@@ -0,0 +1,55 @@
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import ArticulationCfg
+
+WHEELLEG_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="assets/chassis.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=100.0,
+            enable_gyroscopic_forces=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.3),
+        joint_pos={
+            "LeftFrontMotor": 0.0,
+            "LeftBackMotor": 0.0,
+            "RightFrontMotor": 0.0,
+            "RightBackMotor": 0.0,
+            "LeftWheelMotor": 0.0,
+            "RightWheelMotor": 0.0,
+        },
+    ),
+    actuators={
+        "LegMotors": ImplicitActuatorCfg(
+            joint_names_expr=[
+                "LeftFrontMotor",
+                "LeftBackMotor",
+                "RightFrontMotor",
+                "RightBackMotor",
+            ],
+            effort_limit_sim=18.0,
+            velocity_limit_sim=1203.2113,
+            stiffness=25.0,
+            damping=0.01,
+        ),
+        "WheelMotors": ImplicitActuatorCfg(
+            joint_names_expr=["LeftWheelMotor", "RightWheelMotor"],
+            effort_limit_sim=2.462823726,
+            velocity_limit_sim=545.547,
+            stiffness=60.0,
+            damping=1.0,
+        ),
+    },
+)
--- source/wheel_leg_rl/wheel_leg_rl/tasks/manager_based/wheel_leg_rl/wheel_leg_rl_env_cfg.py
+++ source/wheel_leg_rl/wheel_leg_rl/tasks/manager_based/wheel_leg_rl/wheel_leg_rl_env_cfg.py
@@ -23,8 +23,7 @@ from . import mdp
 # Pre-defined configs
 ##
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
-
+from wheel_leg_rl.env_cfg.wheelleg import WHEELLEG_CFG
 
 ##
 # Scene definition
@@ -42,7 +41,7 @@ class WheelLegRlSceneCfg(InteractiveSceneCfg):
     )
 
     # robot
-    robot: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    robot: ArticulationCfg = WHEELLEG_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
     # lights
     dome_light = AssetBaseCfg(
@@ -60,7 +59,9 @@ class WheelLegRlSceneCfg(InteractiveSceneCfg):
 class ActionsCfg:
     """Action specifications for the MDP."""
 
-    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
+    # joint_effort = mdp.JointEffortActionCfg(
+    #     asset_name="robot", joint_names=["slider_to_cart"], scale=100.0
+    # )
 
 
 @configclass
@@ -71,16 +72,16 @@ class ObservationsCfg:
     class PolicyCfg(ObsGroup):
         """Observations for policy group."""
 
-        # observation terms (order preserved)
-        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
+    #     # observation terms (order preserved)
+    #     joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
+    #     joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
 
-        def __post_init__(self) -> None:
-            self.enable_corruption = False
-            self.concatenate_terms = True
+    #     def __post_init__(self) -> None:
+    #         self.enable_corruption = False
+    #         self.concatenate_terms = True
 
-    # observation groups
-    policy: PolicyCfg = PolicyCfg()
+    # # observation groups
+    # policy: PolicyCfg = PolicyCfg()
 
 
 @configclass
@@ -88,66 +89,72 @@ class EventCfg:
     """Configuration for events."""
 
     # reset
-    reset_cart_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
-            "position_range": (-1.0, 1.0),
-            "velocity_range": (-0.5, 0.5),
-        },
-    )
+    # reset_cart_position = EventTerm(
+    #     func=mdp.reset_joints_by_offset,
+    #     mode="reset",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
+    #         "position_range": (-1.0, 1.0),
+    #         "velocity_range": (-0.5, 0.5),
+    #     },
+    # )
 
-    reset_pole_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
-            "position_range": (-0.25 * math.pi, 0.25 * math.pi),
-            "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
-        },
-    )
+    # reset_pole_position = EventTerm(
+    #     func=mdp.reset_joints_by_offset,
+    #     mode="reset",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
+    #         "position_range": (-0.25 * math.pi, 0.25 * math.pi),
+    #         "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
+    #     },
+    # )
 
 
 @configclass
 class RewardsCfg:
     """Reward terms for the MDP."""
 
-    # (1) Constant running reward
-    alive = RewTerm(func=mdp.is_alive, weight=1.0)
-    # (2) Failure penalty
-    terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
-    # (3) Primary task: keep pole upright
-    pole_pos = RewTerm(
-        func=mdp.joint_pos_target_l2,
-        weight=-1.0,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
-    )
-    # (4) Shaping tasks: lower cart velocity
-    cart_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
-    )
-    # (5) Shaping tasks: lower pole angular velocity
-    pole_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.005,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
-    )
+    # # (1) Constant running reward
+    # alive = RewTerm(func=mdp.is_alive, weight=1.0)
+    # # (2) Failure penalty
+    # terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
+    # # (3) Primary task: keep pole upright
+    # pole_pos = RewTerm(
+    #     func=mdp.joint_pos_target_l2,
+    #     weight=-1.0,
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
+    #         "target": 0.0,
+    #     },
+    # )
+    # # (4) Shaping tasks: lower cart velocity
+    # cart_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.01,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
+    # )
+    # # (5) Shaping tasks: lower pole angular velocity
+    # pole_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.005,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
+    # )
 
 
 @configclass
 class TerminationsCfg:
     """Termination terms for the MDP."""
 
-    # (1) Time out
-    time_out = DoneTerm(func=mdp.time_out, time_out=True)
-    # (2) Cart out of bounds
-    cart_out_of_bounds = DoneTerm(
-        func=mdp.joint_pos_out_of_manual_limit,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
-    )
+    # # (1) Time out
+    # time_out = DoneTerm(func=mdp.time_out, time_out=True)
+    # # (2) Cart out of bounds
+    # cart_out_of_bounds = DoneTerm(
+    #     func=mdp.joint_pos_out_of_manual_limit,
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
+    #         "bounds": (-3.0, 3.0),
+    #     },
+    # )
 
 
 ##
